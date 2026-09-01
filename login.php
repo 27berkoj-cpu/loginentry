784e94465bbfb052cb5536d619826b1552a7a9f2
Berko <27BerkoJ@mydacc.org>
2026-09-01 13:37:27 -0400

    updatesssss

--- login.php
+++ login.php
@@ -14,17 +14,20 @@
 <body>
     <div class="login-container">
         <h2 style="text-align: center;">Please Enter Your Login Information</h2>
-        <br>
         <form  style="text-align: center;" method="post">
+            <p class="form-text" >Enter your username 👤</p>
             <input type="text" name="username" placeholder="Username" required>
             <br>
+            <p class="form-text" >Enter your password 🔒</p>
             <input type="password" name="password" placeholder="Password" required>
             <br>
             <button type="submit">Login</button>
         </form>
     </div>
-
+   
+    
     <div class="back-button" style="text-align: center; margin-top: 20px;">
+        <br>
         <a href="home.php">Back to Home</a>
     </div>
 </body>
